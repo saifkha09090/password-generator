ca7b1f691e6d9288bf93fc3241a4670a6d139d7e
Saifullah Khan <92732640+saifkha09090@users.noreply.github.com>
2025-02-02 17:07:03 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+function generatePassword() {
+    let passwordLength = document.getElementById("passwordLength").value;
+    let passwordOutput = document.getElementById("passwordOutput");
+    let includeUppercase = document.getElementById("includeUppercase");
+    let includeLowercase = document.getElementById("includeLowercase");
+    let includeNumbers = document.getElementById("includeNumbers");
+    let includeSymbols = document.getElementById("includeSymbols");
+    let number = "0123456789";
+    let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
+    let characters = "";
+  if (includeUppercase.checked) {
+    characters += upperCase;
+  }
+  if (includeLowercase.checked) {
+    characters += lowerCase;
+  }
+  if (includeNumbers.checked) {
+    characters += number;
+  }
+  if (includeSymbols.checked) {
+    characters += symbols;
+  }
+    if (!includeUppercase.checked && !includeLowercase.checked && !includeNumbers.checked && !includeSymbols.checked) {
+    alert("Please select at least one option");
+    passwordOutput.value = ""
+    return;
+    }
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    let random = Math.round(Math.random() * (characters.length - 1));
+    password += characters[random];
+  }
+  passwordOutput.value = password;
+}
